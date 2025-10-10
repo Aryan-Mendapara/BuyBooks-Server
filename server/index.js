0012744598aa05c8e67440dd5dbfdd0abfa3a3e6
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-10-10 15:52:28 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -13,11 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173", 
+  origin: [    
+          "http://localhost:5173", 
           // "http://localhost:5174",
-          "https://buy-books-twpo.vercel.app"
-          // "https://buybooks-server.onrender.com"
+          "https://buy-books-twpo.vercel.app"          
         ], // React frontend
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 }));
