731b849e7a22a541d88986b53e156556972c44f1
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-10-14 15:23:03 +0530

    send mail

--- server/src/Models/sendMail.js
+++ server/src/Models/sendMail.js
@@ -1,47 +1,22 @@
-// const nodemailer = require('nodemailer');
-
-// const sendEmail = async (to, otp) => {
-//   console.log("send Email");  
-//   const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: process.env.EMAIL,
-//       pass: process.env.EMAIL_PASS
-//     }
-//   });
-
-//   const mailOptions = {
-//     from: process.env.EMAIL,
-//     to,
-//     subject: "Your Login OTP",
-//     text: `Your OTP is: ${otp}`
-//   };
-
-//   await transporter.sendMail(mailOptions);
-// };
-
-// module.exports = sendEmail;
-
-
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, otp) => {
-  console.log("send Email");  
+  console.log("send Email");
 
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS, // Must be App Password if 2FA enabled
-      }
+        pass: process.env.EMAIL_PASS,
+      },
     });
 
     const mailOptions = {
       from: process.env.EMAIL,
       to,
       subject: "Your Login OTP",
-      text: `Your OTP is: ${otp}`
+      text: `Your OTP is: ${otp}`,
     };
 
     const info = await transporter.sendMail(mailOptions);
@@ -49,7 +24,7 @@ const sendEmail = async (to, otp) => {
 
   } catch (err) {
     console.error("Failed to send OTP email:", err.message);
-    throw new Error("Failed to otp send email"); // so backend knows
+    throw new Error("Failed to send OTP email");
   }
 };
 
