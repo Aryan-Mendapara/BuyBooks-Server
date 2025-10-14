43ff43d6d5a689f71b874d691ca0c9dbc03797ef
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-10-14 14:30:51 +0530

    send mail

--- server/src/Models/sendMail.js
+++ server/src/Models/sendMail.js
@@ -1,56 +1,31 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, otp) => {
-  console.log("send Email");  
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  console.log("sendEmail function called");
 
-  const mailOptions = {
-    from: process.env.EMAIL,
-    to,
-    subject: "Your Login OTP",
-    text: `Your OTP is: ${otp}`
-  };
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS, // Must be app password
+      },
+    });
 
-  await transporter.sendMail(mailOptions);
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to,
+      subject: "Your Login OTP",
+      text: `Your OTP is: ${otp}`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully:", info.response);
+
+  } catch (error) {
+    console.error("Error while sending email:", error);
+    throw new Error("Failed to send OTP email");
+  }
 };
 
 module.exports = sendEmail;
-
-
-// const nodemailer = require('nodemailer');
-
-// const sendEmail = async (to, otp) => {
-//   console.log("send Email");  
-
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.EMAIL_PASS, // Must be App Password if 2FA enabled
-//       }
-//     });
-
-//     const mailOptions = {
-//       from: process.env.EMAIL,
-//       to,
-//       subject: "Your Login OTP",
-//       text: `Your OTP is: ${otp}`
-//     };
-
-//     const info = await transporter.sendMail(mailOptions);
-//     console.log("Email sent successfully:", info.response);
-
-//   } catch (err) {
-//     console.error("Failed to send OTP email:", err.message);
-//     throw new Error("Failed to otp send email"); // so backend knows
-//   }
-// };
-
-// module.exports = sendEmail;
