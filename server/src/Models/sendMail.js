88affe286083d45511558ddca6637ce127175b3b
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-10-15 17:12:33 +0530

    send mail

--- server/src/Models/sendMail.js
+++ server/src/Models/sendMail.js
@@ -4,24 +4,23 @@ const sendEmail = async (to, otp) => {
   console.log("📨 Sending OTP email to:", to);
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: "smtp-relay.brevo.com",
+      port: 587,
       auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.BREVO_EMAIL, // your Brevo login email
+        pass: process.env.BREVO_API_KEY,
       },
     });
 
     const mailOptions = {
-      from: `"BuyBooks" <${process.env.EMAIL}>`,
+      from: `"BuyBooks" <${process.env.BREVO_EMAIL}>`,
       to,
       subject: "Your Login OTP",
       text: `Your OTP is ${otp}`,
     };
 
     await transporter.sendMail(mailOptions);
-    console.log("✅ Email sent successfully to:", to);
+    console.log("✅ OTP email sent successfully");
   } catch (error) {
     console.error("❌ Failed to send OTP email:", error.message);
     throw new Error("Email send failed");
