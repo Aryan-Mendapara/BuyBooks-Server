6eb93e7edc6f0349377a8fa752895b4cb4e8a223
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-11-05 17:12:05 +0530

    multer storage, books controller

--- server/src/Middleware/multerStorage.js
+++ server/src/Middleware/multerStorage.js
@@ -1,27 +1,14 @@
-const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
+const multer = require('multer');
 
-// Define the uploads directory (absolute path)
-const uploadDir = path.join(__dirname, "../uploads");
-
-// Create the uploads folder if it doesn’t exist
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-  console.log("✅ 'uploads' folder created at:", uploadDir);
-}
-
-// Configure Multer storage
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir); // use the absolute path
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null,Date.now() + '-' + file.originalname)
+    },
 });
 
-// Initialize the multer instance
 const uploads = multer({ storage });
 
-module.exports = uploads;
+module.exports = uploads
