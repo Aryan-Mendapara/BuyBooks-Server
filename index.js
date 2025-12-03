fba347580ef9ba9832939cdc0628b1b05c88020d
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-12-03 16:42:04 +0530

    changes index

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Static folder (for old local uploads)
-// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/books", index);
 
